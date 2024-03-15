b4cf702e8573bf72fdd41f72317f44e43660ac09
thb-sb <thomas.bailleux@sandboxquantum.com>
2024-03-15 15:00:11 +0100

    Fix #372: expose `hybrid_classical_` and `hybrid_pq_` `OSSL_PARAMS` for `EVP_PKEY`.
    This commit is an attempt to fix #372, by adding four new [`OSSL_PARAM`] to
    [`EVP_PKEY`].
    
    The following [`OSSL_PARAM`] are added by this commit:
      - `hybrid_classical_pub`: an octet string to the classical public key.
      - `hybrid_classical_priv`: an octet string to the classical private key.
      - `hybrid_pq_pub`: an octet string to the quantum-resistant public key.
      - `hybrid_pq_priv`: an octet string to the quantum-resistant private key.
    
    Using [`EVP_PKEY_get_params`], OpenSSL users should be able to extract the
    specific subkey they want from an hybrid key.
    
    A test called `test_evp_pkey_params` has been added to ensure that it works
    with all hybrid algorithms, to also ensure that the output of these parameters
    are consistent between each other.
    
    [`OSSL_PARAM`]: https://www.openssl.org/docs/man3.2/man3/OSSL_PARAM.html
    [`EVP_PKEY`]: https://www.openssl.org/docs/man3.2/man7/evp.html
    [`EVP_PKEY_get_params`]: https://www.openssl.org/docs/man3.2/man3/EVP_PKEY_get_params.html
    
    Signed-off-by: thb-sb <thomas.bailleux@sandboxquantum.com>

--- oqsprov/oqsprov.c
+++ oqsprov/oqsprov.c
@@ -861,8 +861,7 @@ static const OSSL_ALGORITHM oqsprovider_asym_kems[] = {
     ///// OQS_TEMPLATE_FRAGMENT_KEM_FUNCTIONS_END
     {NULL, NULL, NULL}};
 
-static const OSSL_ALGORITHM oqsprovider_keymgmt[]
-    = {
+static const OSSL_ALGORITHM oqsprovider_keymgmt[] = {
 ///// OQS_TEMPLATE_FRAGMENT_KEYMGMT_FUNCTIONS_START
 // clang-format off
 
@@ -1037,9 +1036,9 @@ static const OSSL_ALGORITHM oqsprovider_keymgmt[]
 
     KEMKMHYBALG(p521_hqc256, 256, ecp)
 #endif
-        // clang-format on
-        ///// OQS_TEMPLATE_FRAGMENT_KEYMGMT_FUNCTIONS_END
-        {NULL, NULL, NULL}};
+    // clang-format on
+    ///// OQS_TEMPLATE_FRAGMENT_KEYMGMT_FUNCTIONS_END
+    {NULL, NULL, NULL}};
 
 static const OSSL_ALGORITHM oqsprovider_encoder[] = {
 #define ENCODER_PROVIDER "oqsprovider"
